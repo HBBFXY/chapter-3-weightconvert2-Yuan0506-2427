78c7868f880cd7d5f8f76792bb39b66607e466ea
Yuan0506-2427 <3059560018@qq.com>
2025-09-19 10:51:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# 在这个文件里编写代码
+initial weight=50
+for year in range(1,11);
+    earth weight=initial weight+0.5*year
+    moon weight=earth wweight*0.165
+    print(f"第{year}年,地球体重:
+          {earth weight:2f}公斤,月球体重:
+          {moon weight:2f}公斤")
