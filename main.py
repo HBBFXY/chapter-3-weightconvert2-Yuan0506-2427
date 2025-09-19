190714ed127ae624cbfb63037bedb9184cee385a
Yuan0506-2427 <3059560018@qq.com>
2025-09-19 11:28:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ for year in range(1,11);
     earth weight=initial weight+0.5*year
     moon weight=earth wweight*0.165
     print(f"第{year}年,地球体重:
-          {earth weight:2f}公斤,月球体重:
-          {moon weight:2f}公斤")
+          {earth weight:.2f}公斤,月球体重:
+          {moon weight:.2f}公斤")
